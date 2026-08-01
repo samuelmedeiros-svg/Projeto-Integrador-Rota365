e1a30f6303dc4cc96d1104c4156b1545add5ec4f
Samuel <samuel.medeiros@estudante.iftm.edu.br>
2026-07-31 22:50:42 -0300

    feature: first commit

--- scroll.js
+++ scroll.js
@@ -6,9 +6,9 @@ window.addEventListener('scroll', function() {
 
   if (window.scrollY > 10) {
     header.classList.add('scroll');
-    logo.src ="logo-branca.png";
-    lupa.src ="lupa-branca.png";
-    profile.src ="profile-branca.png";
+    logo.src ="assets/images/logo-branca.png";
+    lupa.src ="assets/imageslupa-branca.png";
+    profile.src ="assets/images/profile-branca.png";
   } else {
     header.classList.remove('scroll');
     logo.src = "logo.png";
