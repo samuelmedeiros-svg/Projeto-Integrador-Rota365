0e846016f01fb6cf74ebf907e57a4d52c0cafc84
Samuel <samuel.medeiros@estudante.iftm.edu.br>
2026-07-31 23:12:09 -0300

    feature: minor updates

--- scroll.js
+++ scroll.js
@@ -7,12 +7,12 @@ window.addEventListener('scroll', function() {
   if (window.scrollY > 10) {
     header.classList.add('scroll');
     logo.src ="assets/images/logo-branca.png";
-    lupa.src ="assets/imageslupa-branca.png";
+    lupa.src ="assets/images/lupa-branca.png";
     profile.src ="assets/images/profile-branca.png";
   } else {
     header.classList.remove('scroll');
-    logo.src = "logo.png";
-    lupa.src ="lupa.png";
-    profile.src ="profile.png";
+    logo.src = "assets/images/logo.png";
+    lupa.src ="assets/images/lupa.png";
+    profile.src ="assets/images/profile.png";
   }
 });
