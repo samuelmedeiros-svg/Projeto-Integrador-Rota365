d50f99ce082264937794fe19c1dad21ff0e976df
Arthur Chaves <arthurchaves190809@gmail.com>
2026-08-11 00:03:27 -0300

    Ajuste nos arquivos

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -6,13 +6,13 @@ window.addEventListener('scroll', function() {
 
   if (window.scrollY > 10) {
     header.classList.add('scroll');
-    logo.src ="assets/images/logo-branca.png";
-    lupa.src ="assets/images/lupa-branca.png";
-    profile.src ="assets/images/profile-branca.png";
+    logo.src ="/assets/images/logo-branca.png";
+    lupa.src ="/assets/images/lupa-branca.png";
+    profile.src ="/assets/images/profile-branca.png";
   } else {
     header.classList.remove('scroll');
-    logo.src = "assets/images/logo.png";
-    lupa.src ="assets/images/lupa.png";
-    profile.src ="assets/images/profile.png";
+    logo.src = "/assets/images/logo.png";
+    lupa.src ="/assets/images/lupa.png";
+    profile.src ="/assets/images/profile.png";
   }
 });
