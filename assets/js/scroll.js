c8bc29c5d48962e1ce5f706b109abc07c655f0f0
Samuel Marques Medeiros <samuel.medeiros@estudante.iftm.edu.br>
2026-09-24 08:28:22 -0300

    Update scroll.js

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -1,21 +1,13 @@
-
-// Pegamos os elementos aqui fora para o navegador não precisar ficar buscando eles toda vez que você rolar a página
-
 const header = document.querySelector('header');
 const logo = document.getElementById('logo');
 const lupa = document.getElementById('lupa');
 const profile = document.getElementById('profile');
-
-// Pega o caminho exato que está no HTML (ex: "assets/images/" ou "../assets/images/")
-// Pegamos do atributo 'src' original e tiramos o "logo.png", sobrando só a rota das pastas!
-
 const caminhoBase = logo.getAttribute('src').replace('logo.png', '');
 
 window.addEventListener('scroll', function () {
   const scrolled = window.scrollY > 10;
 
   header.classList.toggle('scroll', scrolled);
-  // Agora a gente só junta o caminhoBase com o nome da imagem certa
 
   logo.src = scrolled
     ? caminhoBase + 'logo-branca.png'
